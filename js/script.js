4a2ce4ec79ca0e4c807a9f92141d65539b9f5a32
mbashuclovis <mbashuclovis@gmail.com>
2023-03-16 15:56:02 -0800

    Initialize project

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// let menu = document.querySelector("#menu-icon");
+// let navbar = document.querySelector(".navbar");
+
+// menu.addEventListener("click", function () {navbar.classList.toggle("active");});
+
+// window.onscroll = () => {
+//     navbar.classList.remove("active");
+// }
+
+const age = 24;
+const isOldEnough = age >= 24;
+
+if (isOldEnough) {
+    console.log("Clovis can start driving");
+}
